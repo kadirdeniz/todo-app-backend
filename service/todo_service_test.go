5ffc7e7c1d06ca905e7d2ac5cd61f789eaed2d4f
Kadir Deniz <kadir.deniz@ekos.ai>
2022-11-29 21:36:49 +0300

    service test

---
+++ service/todo_service_test.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"todo/mock"
+	"todo/model"
+)
+
+func TestService_CreateTodo(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	todoObj := model.Todo{
+		ID:   "123",
+		Text: "buy some milk",
+	}
+
+	mockRepository := mock.NewMockITodoRepository(ctrl)
+	mockRepository.
+		EXPECT().
+		CreateTodo(
+			gomock.Any(),
+			todoObj,
+		).
+		Return(todoObj).
+		Times(1)
+
+	service := NewTodoService(mockRepository)
+	todo := service.CreateTodo("buy some milk")
+
+	assert.Equal(t, todoObj, todo)
+}
